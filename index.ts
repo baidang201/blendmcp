883e56ad707459f64b9584458462d9a08a9ce5df
Li Smith <jinrishouji@sina.com>
2025-04-20 22:31:02 +0800

    add more ERC20_ABI

--- index.ts
+++ index.ts
@@ -25,8 +25,17 @@ const POOL_ABI = [
 
 // ERC20 ABI
 const ERC20_ABI = [
+  "function balanceOf(address account) view returns (uint256)",
+  "function decimals() view returns (uint8)",
+  "function symbol() view returns (string)",
+  "function name() view returns (string)",
+  "function totalSupply() view returns (uint256)",
   "function approve(address spender, uint256 amount) public returns (bool)",
-  "function allowance(address owner, address spender) view returns (uint256)"
+  "function allowance(address owner, address spender) view returns (uint256)",
+  "function transfer(address recipient, uint256 amount) returns (bool)",
+  "function transferFrom(address sender, address recipient, uint256 amount) returns (bool)",
+  "event Transfer(address indexed from, address indexed to, uint256 value)",
+  "event Approval(address indexed owner, address indexed spender, uint256 value)"
 ];
 
 // 支持的Token配置
